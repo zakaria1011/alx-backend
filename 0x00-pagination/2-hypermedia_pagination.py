a097aa56f5f1e76870bb8dc9a9b2fe0457095ad8
zakaria <grarizakaria@gmail.com>
2024-05-16 15:35:01 +0100

    task 2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -4,7 +4,7 @@
 
 import csv
 import math
-from typing import List,
+from typing import List, Dict, Any
 from typing import Tuple
 
 
@@ -46,3 +46,20 @@ class Server:
             return []
 
         return dataset[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        """ get pagination information with hypermedia"""
+        data = self.get_page(page, page_size)
+        total_items = len(self.dataset())
+        total_pages = math.ceil(total_items/page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages,
+        }
