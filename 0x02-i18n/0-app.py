9322bc74035cbb17fce93d3ebe86691021ba77ad
zakaria <grarizakaria@gmail.com>
2024-05-28 18:20:53 +0100

    al

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ flask app """
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def index():
     """ redner template"""
     return render_template('0-index.html')
