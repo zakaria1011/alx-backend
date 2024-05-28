6a0536d6cf81cd9e014fad09859a5a6553c5969e
zakaria <grarizakaria@gmail.com>
2024-05-28 18:19:51 +0100

    task0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
     """ redner template"""
     return render_template('0-index.html')
