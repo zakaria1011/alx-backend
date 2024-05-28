d4cc39ae156e0a997e9902bd585b19fb5b053b2c
zakaria <grarizakaria@gmail.com>
2024-05-28 22:46:45 +0100

    task1

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -3,8 +3,6 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-app = Flask(__name__)
-
 
 class Config:
     """
@@ -20,6 +18,7 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
