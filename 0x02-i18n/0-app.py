cc14b7c7974e67347b2f20fc965071f292807d72
zakaria <grarizakaria@gmail.com>
2024-05-29 11:35:53 +0100

    alx

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
-""" flask app """
-from flask import Flask, render_template
+"""
+A Basic flask application
+"""
+from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    """ redner template"""
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Renders a basic html template
+    """
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -3,6 +3,7 @@
 A Basic flask application
 """
 from flask import Flask
+from flask import request
 from flask import render_template
 from flask_babel import Babel
 
@@ -24,12 +25,23 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Gets locale from request object
+    """
+    locale = request.args.get('locale', '').strip()
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
     Renders a basic html template
     """
-    return render_template('1-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
